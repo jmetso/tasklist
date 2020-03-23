ec22ef383bbe4ffde2762cb687fc23ffd27c72a6
janne <noreply@example.com>
2020-03-23 12:36:52 +0200

    refactors item methods to separate controller adds item controller test class

---
+++ src/main/java/net/metja/todolist/controller/ItemController.java
@@ -0,0 +1,187 @@
+package net.metja.todolist.controller;
+
+import net.metja.todolist.database.DatabaseManager;
+import net.metja.todolist.database.bean.Repeating;
+import net.metja.todolist.database.bean.Todo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
+
+/**
+ * Rest controller for Task list item methods.
+ *
+ * @author Janne Metso @copy; 2020
+ * @since 2020-03-23
+ */
+@RestController
+public class ItemController {
+
+    private DatabaseManager databaseManager;
+    private static Logger logger = LoggerFactory.getLogger(ItemController.class);
+
+    @PreAuthorize("hasAnyRole('ADMIN','USER', 'VIEW')")
+    @RequestMapping(value = "/api/v1/items", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public ResponseEntity<List<Todo>> getTodoItems(Principal principal) {
+        int listID = this.databaseManager.getUserList(principal.getName());
+        if(listID > 0) {
+            List<Todo> todos = this.databaseManager.getTodos(listID);
+            return new ResponseEntity<>(todos, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @RequestMapping(value="/api/v1/items/add", produces=MediaType.APPLICATION_JSON_VALUE, method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Integer> addTodoListItem(@RequestBody Todo todo, Principal principal) {
+        int listId = this.databaseManager.getUserList(principal.getName());
+        if(listId > 0) {
+            int id = this.databaseManager.addTodo(listId, todo);
+            if(id > 0) {
+                return new ResponseEntity<>(id, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } else {
+            return new ResponseEntity<>(-1, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @RequestMapping(value = "/api/v1/items/{id}/update", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
+    public ResponseEntity updateTodoListItem(@RequestBody Todo todo, @PathVariable(value="id") int id, Principal principal) {
+        int listId = this.databaseManager.getUserList(principal.getName());
+        if(listId > 0) {
+            Todo oldTodo = this.databaseManager.getTodo(listId, id);
+            if(oldTodo != null) {
+                todo.setId(id);
+                if(this.databaseManager.updateTodo(listId, todo)) {
+                    return new ResponseEntity(HttpStatus.OK);
+                } else {
+                    return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+            } else {
+                return new ResponseEntity(HttpStatus.NOT_FOUND);
+            }
+        } else {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @RequestMapping(value = "/api/v1/items/{id}/done", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public ResponseEntity<Boolean> markTodoListItemAsDone(@PathVariable(value="id") int id, Principal principal){
+        int listId = this.databaseManager.getUserList(principal.getName());
+        if(listId > 0) {
+            Todo todo = this.databaseManager.getTodo(listId, id);
+            if(todo != null) {
+                if(!todo.isDone()) {
+                    if(todo.getRepeating() == null || todo.getRepeating() == Repeating.No) {
+                        todo.setDone(true);
+                        if(this.databaseManager.updateTodo(listId, todo)) {
+                            return new ResponseEntity<>(true, HttpStatus.OK);
+                        } else {
+                            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+                        }
+                    } else {
+                        switch (todo.getRepeating()) {
+                            case Daily: todo.setDueDate(todo.getDueDate().plusDays(1)); break;
+                            case Weekly: todo.setDueDate(todo.getDueDate().plusWeeks(1)); break;
+                            case BiWeekly: todo.setDueDate(todo.getDueDate().plusWeeks(2)); break;
+                            case Monthly: todo.setDueDate(todo.getDueDate().plusMonths(1)); break;
+                            case Yearly: todo.setDueDate(todo.getDueDate().plusYears(1)); break;
+                        }
+                        this.databaseManager.updateTodo(listId, todo);
+                        return new ResponseEntity<>(true, HttpStatus.OK);
+                    }
+                } else {
+                    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                }
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @RequestMapping(value = "/api/v1/items/{id}/activate", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public ResponseEntity<Boolean> markTodoListItemAsActive(@PathVariable(value="id") int id, Principal principal){
+        int listId = this.databaseManager.getUserList(principal.getName());
+        if(listId > 0) {
+            Todo todo = this.databaseManager.getTodo(listId, id);
+            if(todo != null) {
+                logger.debug("Scheduled: "+todo.isScheduled()+" Repeating: "+todo.getRepeating());
+                if(todo.isDone()) {
+                    todo.setDone(false);
+                    if(this.databaseManager.updateTodo(listId, todo)) {
+                        return new ResponseEntity<>(true, HttpStatus.OK);
+                    } else {
+                        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+                    }
+                } else {
+                    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                }
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @RequestMapping(value = "/api/v1/items/{id}/deactivate", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public ResponseEntity<Boolean> markTodoListItemAsInactive(@PathVariable(value="id") int id, Principal principal){
+        int listId = this.databaseManager.getUserList(principal.getName());
+        if(listId > 0) {
+            Todo todo = this.databaseManager.getTodo(listId, id);
+            if(todo != null) {
+                if(!todo.isDone() && todo.isScheduled() && todo.getRepeating() != null && todo.getRepeating() != Repeating.No) {
+                    todo.setDone(true);
+                    if (this.databaseManager.updateTodo(listId, todo)) {
+                        return new ResponseEntity<>(true, HttpStatus.OK);
+                    } else {
+                        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+                    }
+                } else {
+                    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                }
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @RequestMapping(value = "/api/v1/items/{id}/delete", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public ResponseEntity<Boolean> deleteTodoListItem(@PathVariable(value="id") int id, Principal principal) {
+        int listId = this.databaseManager.getUserList(principal.getName());
+        if(listId > 0) {
+            if(this.databaseManager.deleteTodo(listId, id)) {
+                return new ResponseEntity<>(true, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
+            }
+        } else {
+            return new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Autowired
+    void setDatabaseManager(DatabaseManager databaseManager) {
+        this.databaseManager = databaseManager;
+    }
+
+}
