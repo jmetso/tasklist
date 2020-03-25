a9acf3c745e37907389915009e68072d0de05e6b
janne <noreply@example.com>
2020-03-25 10:23:54 +0200

    code cleanups

--- src/main/java/net/metja/todolist/notification/EmailClient.java
+++ src/main/java/net/metja/todolist/notification/EmailClient.java
@@ -32,15 +32,14 @@ public class EmailClient implements NotificationClient {
         final String SMTP_SERVER = configUtil.getSMTPServer();
         final String USERNAME = configUtil.getSMTPUsername();
         final String PASSWORD = configUtil.getSMTPPassword();
-
         final String EMAIL_FROM = configUtil.getFromEmail();
         final String EMAIL_TO = USER.getEmail();
 
         Properties prop = System.getProperties();
-        prop.put("mail.smtp.host", SMTP_SERVER); //optional, defined in SMTPTransport
+        prop.put("mail.smtp.host", SMTP_SERVER);
         prop.put("mail.smtp.auth", "true");
-        prop.put("mail.smtp.port", ""+configUtil.getSMTPPort()); // default port 25
-        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+        prop.put("mail.smtp.port", ""+configUtil.getSMTPPort());
+        prop.put("mail.smtp.starttls.enable", "true");
         prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
         Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
@@ -51,34 +50,19 @@ public class EmailClient implements NotificationClient {
         Message msg = new MimeMessage(session);
 
         try {
-
-            // from
             msg.setFrom(new InternetAddress(EMAIL_FROM));
-
-            // to
             msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(EMAIL_TO, false));
-
-            // subject
             msg.setSubject(SUBJECT);
-
-            // content
             msg.setText(MESSAGE);
-
             msg.setSentDate(new Date());
 
-            // Get SMTPTransport
             SMTPTransport t = (SMTPTransport) session.getTransport("smtps");
             logger.debug("Connecting to email server");
-            // connect
             t.connect(SMTP_SERVER, USERNAME, PASSWORD);
-
-            // send
             t.sendMessage(msg, msg.getAllRecipients());
-
             logger.debug("Response: " + t.getLastServerResponse());
 
             t.close();
-
         } catch (MessagingException e) {
             e.printStackTrace();
         }
